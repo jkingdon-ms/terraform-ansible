dfa2cd1da37bb78f47da01ec5afad3053aeabb74
James Kingdon <jameskingdon@.microsoft.com>
2023-01-03 15:44:38 -0500

    try referencing ansible directory

--- manual-run.sh
+++ manual-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 terraform apply
-ansible-playbook -u `terraform output admin_username` -i `terraform output host`, --private-key ~/.ssh/id_rsa ansible-playbook.yml
+ansible-playbook -u `terraform output admin_username` -i `terraform output host`, --private-key ~/.ssh/id_rsa ansible/playbook.yml
